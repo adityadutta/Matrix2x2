298887265770f597c7d33736ad4fe7d5f105b11b
Aditya Dutta <aditya.dutta10@gmail.com>
2017-12-11 18:18:59 -0500

    Initial commit

---
+++ Assignment4/Matrix2x2.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment4
+{
+    class Matrix2x2
+    {
+        double e11, e12, e21, e22;
+        //Getter and setter for e11
+        double E11
+        {
+            get { return e11; }
+            set { e11 = value; }
+        }
+        //Getter and setter for e12
+        double E12
+        {
+            get { return e12; }
+            set { e12 = value; }
+        }
+        //Getter and setter for e21
+        double E21
+        {
+            get { return e21; }
+            set { e21 = value; }
+        }
+        //Getter and setter for e22
+        double E22
+        {
+            get { return e22; }
+            set { e22 = value; }
+        }
+
+        //defualt constructor
+        public Matrix2x2()
+        {
+            SetIdentityMatrix();
+        }
+
+        //parameterised  constructor
+        public Matrix2x2(double _e11, double _e12, double _e21, double _e22)
+        {
+            e11 = _e11;
+            e12 = _e12;
+            e21 = _e21;
+            e22 = _e22;
+        }
+
+        public void SetIdentityMatrix()
+        {
+            e11 = e22 = 1;
+            e12 = e21 = 0;
+        }
+
+        public void SetRotationMatrixInDegrees(double angle)
+        {
+            angle *= Math.PI / 180;
+            e11 = Math.Cos(angle) * (180/Math.PI);
+            e12 = -Math.Sin(angle) * (180 / Math.PI);
+            e21 = Math.Sin(angle) * (180 / Math.PI);
+            e22 = Math.Cos(angle) * (180 / Math.PI);
+        }
+
+        public void SetScalingMatrix(double scaleX, double scaleY)
+        {
+            double _e11, _e12, _e21, _e22;
+            _e11 = e11 * scaleX;
+            _e21 = e21 * scaleX;
+            _e12 = e12 * scaleY;
+            _e22 = e22 * scaleY;
+
+            Console.WriteLine(_e11 + "    " + _e12 + "\n" + _e21 + "    " + _e22);
+        }
+
+        //Add two matrices
+        public void AddMatrix(Matrix2x2 otherMatrix)
+        {
+            double _e11, _e12, _e21, _e22;
+            _e11 = e11 + otherMatrix.e11;
+            _e12 = e12 + otherMatrix.e12;
+            _e21 = e21 + otherMatrix.e21;
+            _e22 = e22 + otherMatrix.e22;
+            Console.WriteLine(_e11 + "    " + _e12 + "\n" + _e21 + "    " + _e22);
+        }
+
+        //Subtract two matrices
+        public void SubtractMatrix(Matrix2x2 otherMatrix)
+        {
+            double _e11, _e12, _e21, _e22;
+            _e11 = e11 - otherMatrix.e11;
+            _e12 = e12 - otherMatrix.e12;
+            _e21 = e21 - otherMatrix.e21;
+            _e22 = e22 - otherMatrix.e22;
+            Console.WriteLine(_e11 + "    " + _e12 + "\n" + _e21 + "    " + _e22);
+        }
+
+    }
+}
---
+++ Assignment4/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int op; // variable to hold option
+                char ch; // var to hold choice
+                double e11, e12, e21, e22; //vars to hold vector values and scalar value   
+                Matrix2x2 matrix = new Matrix2x2();        
+                do
+                {
+                    //user input for matrix2x2
+                    Console.Write("Input a value for row 1 and column 1: ");
+                    e11 = Convert.ToDouble(Console.ReadLine());
+                    Console.Write("Input a value for row 1 and column 2: ");
+                    e12 = Convert.ToDouble(Console.ReadLine());
+                    Console.Write("Input a value for row 2 and column 1: ");
+                    e21 = Convert.ToDouble(Console.ReadLine());
+                    Console.Write("Input a value for row 2 and column 2: ");
+                    e22 = Convert.ToDouble(Console.ReadLine());
+
+                    matrix = new Matrix2x2(e11, e12, e21, e22);
+
+                    Console.WriteLine(e11 + "    " + e12 + "\n" + e21 + "    " + e22);
+                    //Options
+                    Console.WriteLine("Matrix2x2\n1.Add Matrix\n2.Subtract Matrix\n3.Set Scaling Matrix\n4.Quit");
+                    Console.WriteLine("\nEnter your choice: "); //user option
+                    op = Convert.ToInt32(Console.ReadLine());
+                    //switching the options
+                    switch (op)
+                    {
+                        case 1:
+                            Console.Write("Input a value for row 1 and column 1: ");
+                            e11 = Convert.ToDouble(Console.ReadLine());
+                            Console.Write("Input a value for row 1 and column 2: ");
+                            e12 = Convert.ToDouble(Console.ReadLine());
+                            Console.Write("Input a value for row 2 and column 1: ");
+                            e21 = Convert.ToDouble(Console.ReadLine());
+                            Console.Write("Input a value for row 2 and column 2: ");
+                            e22 = Convert.ToDouble(Console.ReadLine());
+
+                            Matrix2x2 otherMatrix = new Matrix2x2(e11, e12, e21, e22);
+                            matrix.AddMatrix(otherMatrix);
+                            break;
+                        case 2:
+                            Console.Write("Input a value for row 1 and column 1: ");
+                            e11 = Convert.ToDouble(Console.ReadLine());
+                            Console.Write("Input a value for row 1 and column 2: ");
+                            e12 = Convert.ToDouble(Console.ReadLine());
+                            Console.Write("Input a value for row 2 and column 1: ");
+                            e21 = Convert.ToDouble(Console.ReadLine());
+                            Console.Write("Input a value for row 2 and column 2: ");
+                            e22 = Convert.ToDouble(Console.ReadLine());
+
+                            Matrix2x2 otherMatrix1 = new Matrix2x2(e11, e12, e21, e22);
+                            matrix.SubtractMatrix(otherMatrix1);
+                            break;
+                        case 3:
+                            double scaleX, scaleY;
+                            Console.Write("Enter value to Scale X: ");
+                            scaleX = Convert.ToDouble(Console.ReadLine());
+                            Console.Write("Enter value to Scale Y: ");
+                            scaleY = Convert.ToDouble(Console.ReadLine());
+
+                            matrix.SetScalingMatrix(scaleX, scaleY);
+                            break;
+                        case 4:
+                            //exit the applications
+                            Environment.Exit(0);
+                            break;
+                        default:
+                            //if invalid input is entered
+                            Console.WriteLine("Invalid Input! Try again.");
+                            break;
+                    }
+                    //users' choice to continue
+                    Console.WriteLine("\nContinue?(y/n)");
+                    ch = Convert.ToChar(Console.ReadLine());
+                }
+                //if it is 'yes' continue the program
+                while (ch == 'y' || ch == 'Y');
+            }
+            //catching exception and displaying error
+            catch (Exception e)
+            {
+                Console.WriteLine("Error encountered: " + e.Message + "\nRun the program again! Press any key.");
+                Console.ReadKey();
+            }
+
+        }
+    }
+}
